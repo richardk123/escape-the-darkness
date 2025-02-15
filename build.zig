2a6626f26a77ea2f570ebfddda9733cb2a94083f
richardkolisek <richard.kolisek@eskerpartners.com>
2025-02-15 12:52:43 +0100

    add system libs

--- build.zig
+++ build.zig
@@ -66,4 +66,34 @@ pub fn build(b: *std.Build) void {
         .install_subdir = b.pathJoin(&.{ "bin", content_dir }),
     });
     exe.step.dependOn(&install_content_step.step);
+
+    switch (target.result.os.tag) {
+        .windows => {
+            if (target.result.cpu.arch.isX86()) {
+                if (target.result.abi.isGnu() or target.result.abi.isMusl()) {
+                    if (b.lazyDependency("system_sdk", .{})) |system_sdk| {
+                        exe.addLibraryPath(system_sdk.path("windows/lib/x86_64-windows-gnu"));
+                    }
+                }
+            }
+        },
+        .macos => {
+            if (b.lazyDependency("system_sdk", .{})) |system_sdk| {
+                exe.addLibraryPath(system_sdk.path("macos12/usr/lib"));
+                exe.addFrameworkPath(system_sdk.path("macos12/System/Library/Frameworks"));
+            }
+        },
+        .linux => {
+            if (target.result.cpu.arch.isX86()) {
+                if (b.lazyDependency("system_sdk", .{})) |system_sdk| {
+                    exe.addLibraryPath(system_sdk.path("linux/lib/x86_64-linux-gnu"));
+                }
+            } else if (target.result.cpu.arch == .aarch64) {
+                if (b.lazyDependency("system_sdk", .{})) |system_sdk| {
+                    exe.addLibraryPath(system_sdk.path("linux/lib/aarch64-linux-gnu"));
+                }
+            }
+        },
+        else => {},
+    }
 }
