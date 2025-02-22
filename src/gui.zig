27fb1b74da06c644f2d73012eb9e4690ef08f556
richardkolisek <richard.kolisek@eskerpartners.com>
2025-02-22 13:22:26 +0100

    more refactoring

--- src/gui.zig
+++ src/gui.zig
@@ -1,9 +1,39 @@
+const std = @import("std");
+const math = std.math;
 const zgui = @import("zgui");
+const zglfw = @import("zglfw");
+const zgpu = @import("zgpu");
+const wgpu = zgpu.wgpu;
+
 const GPUEngine = @import("gpu_engine.zig").GPUEngine;
+const content_dir = @import("build_options").content_dir;
 
 pub const GUI = struct {
-    pub fn init() GUI {
+    pub fn init(allocator: std.mem.Allocator, window: *zglfw.Window, gpuEngine: *GPUEngine) GUI {
+        zgui.init(allocator);
+
+        const scale_factor = scale_factor: {
+            const scale = window.getContentScale();
+            break :scale_factor @max(scale[0], scale[1]);
+        };
+
+        _ = zgui.io.addFontFromFile(content_dir ++ "Roboto-Medium.ttf", math.floor(16.0 * scale_factor));
+
+        zgui.backend.init(
+            window,
+            gpuEngine.gctx.device,
+            @intFromEnum(zgpu.GraphicsContext.swapchain_format),
+            @intFromEnum(wgpu.TextureFormat.undef),
+        );
+
+        zgui.getStyle().scaleAllSizes(scale_factor);
         return GUI{};
+    }
+
+    pub fn deinit(self: *GUI) void {
+        _ = self;
+        zgui.backend.deinit();
+        zgui.deinit();
     }
 
     pub fn update(self: *GUI, gpuEngine: *GPUEngine) void {
--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,8 @@
 const std = @import("std");
-const math = std.math;
 const zglfw = @import("zglfw");
-const zgpu = @import("zgpu");
-const wgpu = zgpu.wgpu;
-const zgui = @import("zgui");
-const zm = @import("zmath");
-const zmesh = @import("zmesh");
 const mesh_loader = @import("mesh_loader.zig");
 const GPUEngine = @import("gpu_engine.zig").GPUEngine;
 const GUI = @import("gui.zig").GUI;
-const content_dir = @import("build_options").content_dir;
 const window_title = "Escape the darkness";
 
 pub fn main() !void {
@@ -31,33 +24,15 @@ pub fn main() !void {
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
-
     const allocator = gpa.allocator();
+
     try mesh_loader.loadMeshes(allocator);
 
     var gpuEngine = try GPUEngine.init(allocator, window);
     defer gpuEngine.deinit(allocator);
-    var gui = GUI.init();
 
-    const scale_factor = scale_factor: {
-        const scale = window.getContentScale();
-        break :scale_factor @max(scale[0], scale[1]);
-    };
-
-    zgui.init(allocator);
-    defer zgui.deinit();
-
-    _ = zgui.io.addFontFromFile(content_dir ++ "Roboto-Medium.ttf", math.floor(16.0 * scale_factor));
-
-    zgui.backend.init(
-        window,
-        gpuEngine.gctx.device,
-        @intFromEnum(zgpu.GraphicsContext.swapchain_format),
-        @intFromEnum(wgpu.TextureFormat.undef),
-    );
-    defer zgui.backend.deinit();
-
-    zgui.getStyle().scaleAllSizes(scale_factor);
+    var gui = GUI.init(allocator, window, &gpuEngine);
+    defer gui.deinit();
 
     while (!window.shouldClose() and window.getKey(.escape) != .press) {
         zglfw.pollEvents();
