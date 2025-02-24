7086047fa203f7e77e6dfe563c8dfea6be2c8a86
richardkolisek <richard.kolisek@eskerpartners.com>
2025-02-24 16:20:54 +0100

    add echolocation renderer

---
+++ src/renderer/gpu_engine.zig
@@ -0,0 +1,141 @@
+const std = @import("std");
+const zgpu = @import("zgpu");
+const wgpu = zgpu.wgpu;
+const zglfw = @import("zglfw");
+const zm = @import("zmath");
+const math = std.math;
+const zgui = @import("zgui");
+
+const TextureView = wgpu.TextureView;
+const CommandEncoder = wgpu.CommandEncoder;
+
+const Meshes = @import("mesh_loader.zig").Meshes;
+const Mesh = @import("mesh_loader.zig").Mesh;
+const Vertex = @import("mesh_loader.zig").Vertex;
+const EcholocationProgram = @import("echolocation/echolocation.zig").EcholocationProgram;
+
+pub const RenderParams = struct {
+    gctx: *zgpu.GraphicsContext,
+    meshes: *Meshes,
+    back_buffer_view: TextureView,
+    encoder: CommandEncoder,
+    depth_texture_view: zgpu.TextureViewHandle,
+};
+
+pub const GPUEngine = struct {
+    gctx: *zgpu.GraphicsContext,
+    echolocationProgram: EcholocationProgram,
+    depth_texture: zgpu.TextureHandle,
+    depth_texture_view: zgpu.TextureViewHandle,
+
+    pub fn init(allocator: std.mem.Allocator, window: *zglfw.Window, meshes: *Meshes) !GPUEngine {
+        const gctx = try zgpu.GraphicsContext.create(
+            allocator,
+            .{
+                .window = window,
+                .fn_getTime = @ptrCast(&zglfw.getTime),
+                .fn_getFramebufferSize = @ptrCast(&zglfw.Window.getFramebufferSize),
+                .fn_getWin32Window = @ptrCast(&zglfw.getWin32Window),
+                .fn_getX11Display = @ptrCast(&zglfw.getX11Display),
+                .fn_getX11Window = @ptrCast(&zglfw.getX11Window),
+                .fn_getWaylandDisplay = @ptrCast(&zglfw.getWaylandDisplay),
+                .fn_getWaylandSurface = @ptrCast(&zglfw.getWaylandWindow),
+                .fn_getCocoaWindow = @ptrCast(&zglfw.getCocoaWindow),
+            },
+            .{},
+        );
+        errdefer gctx.destroy(allocator);
+
+        const echolocationProgram = try EcholocationProgram.init(gctx, meshes);
+        // Create a depth texture and its 'view'.
+        const depth = createDepthTexture(gctx);
+
+        return GPUEngine{
+            .gctx = gctx,
+            .echolocationProgram = echolocationProgram,
+            .depth_texture = depth.texture,
+            .depth_texture_view = depth.view,
+        };
+    }
+
+    pub fn deinit(self: *GPUEngine, allocator: std.mem.Allocator) void {
+        self.gctx.destroy(allocator);
+    }
+
+    pub fn draw(self: *GPUEngine, meshes: *Meshes) void {
+        const gctx = self.gctx;
+
+        const back_buffer_view = gctx.swapchain.getCurrentTextureView();
+        defer back_buffer_view.release();
+
+        const commands = commands: {
+            const encoder = gctx.device.createCommandEncoder(null);
+            defer encoder.release();
+
+            const renderParams = RenderParams{
+                .gctx = gctx,
+                .meshes = meshes,
+                .back_buffer_view = back_buffer_view,
+                .encoder = encoder,
+                .depth_texture_view = self.depth_texture_view,
+            };
+
+            self.echolocationProgram.render(renderParams);
+            // gui pass
+            {
+                const color_attachments = [_]wgpu.RenderPassColorAttachment{.{
+                    .view = back_buffer_view,
+                    .load_op = .load,
+                    .store_op = .store,
+                }};
+                const render_pass_info = wgpu.RenderPassDescriptor{
+                    .color_attachment_count = color_attachments.len,
+                    .color_attachments = &color_attachments,
+                };
+                const pass = encoder.beginRenderPass(render_pass_info);
+                defer {
+                    pass.end();
+                    pass.release();
+                }
+
+                zgui.backend.draw(pass);
+            }
+
+            break :commands encoder.finish(null);
+        };
+        defer commands.release();
+
+        gctx.submit(&.{commands});
+
+        if (gctx.present() == .swap_chain_resized) {
+            // Release old depth texture.
+            gctx.releaseResource(self.depth_texture_view);
+            gctx.destroyResource(self.depth_texture);
+
+            // Create a new depth texture to match the new window size.
+            const depth = createDepthTexture(gctx);
+            self.depth_texture = depth.texture;
+            self.depth_texture_view = depth.view;
+        }
+    }
+
+    fn createDepthTexture(gctx: *zgpu.GraphicsContext) struct {
+        texture: zgpu.TextureHandle,
+        view: zgpu.TextureViewHandle,
+    } {
+        const texture = gctx.createTexture(.{
+            .usage = .{ .render_attachment = true },
+            .dimension = .tdim_2d,
+            .size = .{
+                .width = gctx.swapchain_descriptor.width,
+                .height = gctx.swapchain_descriptor.height,
+                .depth_or_array_layers = 1,
+            },
+            .format = .depth32_float,
+            .mip_level_count = 1,
+            .sample_count = 1,
+        });
+        const view = gctx.createTextureView(texture, .{});
+        return .{ .texture = texture, .view = view };
+    }
+};
---
+++ src/renderer/mesh_loader.zig
@@ -0,0 +1,87 @@
+const zmesh = @import("zmesh");
+const std = @import("std");
+const expect = std.testing.expect;
+
+pub const Vertex = extern struct {
+    position: [3]f32,
+    normal: [3]f32,
+};
+
+pub const Mesh = struct {
+    index_offset: u32,
+    vertex_offset: i32,
+    num_indices: u32,
+    num_vertices: u32,
+};
+
+pub const Meshes = struct {
+    meshes: std.ArrayList(Mesh),
+    vertices: std.ArrayList(Vertex),
+    indices: std.ArrayList(u32),
+
+    pub fn init(allocator: std.mem.Allocator) !Meshes {
+        zmesh.init(allocator);
+        defer zmesh.deinit();
+
+        const data = try zmesh.io.zcgltf.parseAndLoadFile("content/" ++ "monkey.gltf");
+        defer zmesh.io.zcgltf.freeData(data);
+
+        var mesh_indices = std.ArrayList(u32).init(allocator);
+        var mesh_positions = std.ArrayList([3]f32).init(allocator);
+        var mesh_normals = std.ArrayList([3]f32).init(allocator);
+        defer mesh_indices.deinit();
+        defer mesh_positions.deinit();
+        defer mesh_normals.deinit();
+
+        try zmesh.io.zcgltf.appendMeshPrimitive(
+            data,
+            0, // mesh index
+            0, // gltf primitive index (submesh index)
+            &mesh_indices,
+            &mesh_positions,
+            &mesh_normals, // normals (optional)
+            null, // texcoords (optional)
+            null, // tangents (optional)
+        );
+
+        // std.debug.print(" \n \nindices {any} \n \n", .{mesh_indices.items});
+        // std.debug.print(" \n \npositions {any} \n \n", .{mesh_positions.items});
+        // std.debug.print(" \n \nnormals {any} \n \n", .{mesh_normals.items});
+
+        var meshes = std.ArrayList(Mesh).init(allocator);
+        var vertices = std.ArrayList(Vertex).init(allocator);
+        var indices = std.ArrayList(u32).init(allocator);
+
+        try meshes.append(.{
+            .index_offset = 0,
+            .vertex_offset = 0,
+            .num_indices = @as(u32, @intCast(mesh_indices.items.len)),
+            .num_vertices = @as(u32, @intCast(mesh_positions.items.len)),
+        });
+
+        try vertices.ensureTotalCapacity(mesh_positions.items.len);
+        for (mesh_positions.items, 0..) |_, index| {
+            vertices.appendAssumeCapacity(.{
+                .position = mesh_positions.items[index],
+                .normal = mesh_normals.items[index],
+            });
+        }
+
+        try indices.ensureTotalCapacity(mesh_indices.items.len);
+        for (mesh_indices.items) |mesh_index| {
+            indices.appendAssumeCapacity(mesh_index);
+        }
+
+        return Meshes{
+            .meshes = meshes,
+            .vertices = vertices,
+            .indices = indices,
+        };
+    }
+
+    pub fn deinit(self: *Meshes) void {
+        self.meshes.deinit();
+        self.vertices.deinit();
+        self.indices.deinit();
+    }
+};
